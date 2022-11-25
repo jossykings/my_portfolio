803b90bf85d57a5f5fcfd13a3d6d875c135f0a5f
jossyking <abia2020joshua@gmail.com>
2022-11-25 23:01:52 +0100

    debbieproject

--- app.js
+++
@@ -1,9 +0,0 @@
-const see = document.querySelector("#hero");
-const Option = {};
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    console.log(entry.target.id);
-  });
-}, Option);
-
-observer.observe(see);
